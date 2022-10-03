96887c4d7455654820d18e50b48f7c73b9c35b24
Ir. Ignasius Axel Hutomo, S.T. M.Sc., IPP <axelhutomo@gmail.com>
2022-10-03 12:24:38 +0200

    __init__

---
+++ Urban Modelling/Hello.py
@@ -0,0 +1,12 @@
+print('Hello World')
+print('axel')
+
+#a = 10
+#b = 20
+#c = 30 
+
+#a = input('Please insert a value for A variable')
+#b = input('Please insert a value for B variable')
+#c = input('Please insert a value for C variable')
+#print(a)
+#x = (-b + (b**2 - 4 * a * c)**0.5) / 2 * a
